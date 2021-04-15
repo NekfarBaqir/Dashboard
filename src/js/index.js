317fe6ba7e523a531e68e28c5c6c3fac2f9e2c43
NekfarBaqir <m.nekfar.baqir.786@gmail.com>
2021-04-15 06:39:50 -0800

    This is the first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// initializing the jQuery
+$(document).ready(function () {
+  // code here...
+  // notificaiton box
+
+  $(".notify-cover").click(function () {
+    $(".notification-box").toggleClass("bad");
+  });
+});
